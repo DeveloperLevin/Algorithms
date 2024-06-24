d83119e6186b513b400eb986c3911bcacbb715f4
Levin Samson Moras <107058217+DeveloperLevin@users.noreply.github.com>
2024-06-24 08:35:58 +0000

    program to recursively print numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+  // Function to recursively print numbers from till the value of n is equal to 0
+  public static void printNumb(int n) {
+    if (n == 0) {
+      return;
+    }
+    System.out.println(n);
+    printNumb(n - 1);
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    printNumb(n);
   }
 }
